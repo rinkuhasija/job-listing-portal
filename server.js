a7002d3b3898e318ff1be1f20a3c9a10b045c889
Rinku Hasija <rishabhasija5454@gmail.com>
2023-04-17 19:57:42 +0530

    added login/register routes and jwt tokenization

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config()
 const connectDB = require('./config/db');
 const User = require("./model/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -20,6 +22,91 @@ app.get('/', async (req, res) => {
     res.status(500).send('Server Error', err);
   }
 });
+
+app.post('/register', async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+
+    //validate input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+    user.save();
+
+    res.status(201).json(user);
+
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post('/login', async (req,res) => {
+
+  try {
+    const {email, password } = req.body;
+
+    //validate input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user exist in our database
+    const user = await User.findOne({ email: email });
+
+    if(!user) {
+      res.status(404).send("User not found");
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      res.status(200).json(token);
+    
+  } else {
+    res.status(400).send("Invalid Credentials");
+  }} catch (err) {
+    console.log(err);
+  }
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port 3000');
